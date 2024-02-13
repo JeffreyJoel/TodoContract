0c7e60fc88024b33771e648ee41556ed9691619c
JeffreyJoel <jowoloko@gmail.com>
2024-02-13 01:16:56 +0100

    add todo struct

---
+++ contracts/Todo.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract Todo {
+    struct toDo {
+        string title;
+        string description;
+        bool completed;
+    }
+
+}
+
